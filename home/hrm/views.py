b9f8dd70a25c58be287791a5c8c5d6b2d5597acd
dantechdevs <damnngwasi@gmail.com>
2025-04-08 22:15:23 +0300

    added admin management

--- home/hrm/views.py
+++ home/hrm/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+    
+def manage(request):
+    return render(request, 'manage.html', {})
+
     
